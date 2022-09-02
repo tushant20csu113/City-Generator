89aef4d65d89f86504b5351ce3e5f8fa4bd4563e
tushant20csu113 <101614715+tushant20csu113@users.noreply.github.com>
2022-09-02 09:24:59 +0530

    -oops

--- Assets/Scripts/Racing/Cars.cs
+++ Assets/Scripts/Racing/Cars.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="Car",menuName ="Cars/Create New")]
 public class Cars : ScriptableObject
 {
+    public Sprite[] characters;
     public string carName;
     public Color carColor;
     public int topSpeed;
---
+++ Assets/Scripts/Racing/EnemyCarMovement.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCarMovement : MonoBehaviour
+{
+    [SerializeField] List<Transform> waypoints;
+
+    [SerializeField] float enemySpeed = 2f;
+
+    int waypointIndex = 0;
+
+    private void Start()
+    {
+        transform.position = waypoints[waypointIndex].position; // Starting enemycar position = 1st waypoint.
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green; //Start point in the scene
+        Gizmos.DrawSphere(waypoints[0].position, 0.5f);
+
+        Gizmos.color = Color.green;
+        for(int i = 1; i < waypoints.Count; i++)
+        {
+            Gizmos.DrawSphere(waypoints[i].position, 0.5f);
+        }
+
+        Gizmos.color = Color.blue;
+        for(int i = 1; i < waypoints.Count; i++)
+        {
+            Gizmos.DrawLine(waypoints[i - 1].position, waypoints[i].position);
+        }
+
+        Gizmos.color = Color.red; // Finish Point in the scene
+        Gizmos.DrawSphere(waypoints[waypoints.Count - 1].position, 0.5f);
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex].position;
+            var move = enemySpeed * Time.deltaTime;
+
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, move);
+
+            if (Vector3.Distance(transform.position, waypoints[waypointIndex].position) < 0.1f)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+            waypointIndex = 0;
+    }
+}
