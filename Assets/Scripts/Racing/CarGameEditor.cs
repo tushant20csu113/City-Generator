4ae8569a1527b679055d66a33985e35655c1f9cc
tushant20csu113 <101614715+tushant20csu113@users.noreply.github.com>
2022-09-08 09:19:01 +0530

    -Editor window

---
+++ Assets/Scripts/Racing/CarGameEditor.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CarGameEditor : EditorWindow
+{
+    string carName;
+    Sprite carImage;
+    [MenuItem("Racing/Car Game/Game Info")]
+    static void OpenWindow()
+    {
+        CarGameEditor myWindow = (CarGameEditor)GetWindow(typeof(CarGameEditor));
+        myWindow.Show();
+    }
+    private void OnGUI()
+    {
+        GUILayout.Label("This is my cutom editor window.");
+        carName = EditorGUILayout.TextField("Car Name", carName);
+        //(Sprite)EditorGUILayout.ObjectField("Head", player.headSprite);
+        carImage = (Sprite)EditorGUILayout.ObjectField("Car Image", carImage, typeof(Sprite));
+        if (GUILayout.Button("Create Car"))
+        {
+            CarsData car = ScriptableObject.CreateInstance<CarsData>();
+            car.carName = carName;
+            car.carImage = carImage;
+            AssetDatabase.CreateAsset(car, "Assets/Scripts/Racing/Cars/" + carName + ".asset");
+            AssetDatabase.SaveAssets();
+        }
+    }
+}
