af895f93c0ec7fe6fef3461afa844d7b33ad891b
tushant20csu113 <101614715+tushant20csu113@users.noreply.github.com>
2022-09-09 09:17:38 +0530

    -car

--- Assets/Scripts/Racing/Rotator.cs
+++ Assets/Scripts/Racing/Rotator.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
+    public Transform car;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class Rotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0,10*Time.deltaTime, 0, Space.Self);
+        car.Rotate(0,10*Time.deltaTime, 0, Space.Self);
     }
 }
